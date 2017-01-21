cb6fd82955e7e5a94772c149ce3de0efa3e84d79
pnemrow <pnemrow@gmail.com>
2017-01-21 01:52:19 -0700

    Left and Right included, Removal of Zeros
    Included the left and right camera angle images and adjusted steering
    positions. Found a lot of zero valued steering angles in the training
    data and removed 90% of them to remove bias toward non-turning.

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 from pandas import read_csv as csv
+import random
 from scipy.misc import imread, imresize
 import numpy as np
 from keras.models import Sequential
@@ -7,27 +8,38 @@ from keras.layers.advanced_activations import ELU
 from keras.layers.normalization import BatchNormalization
 from keras.optimizers import SGD
 from sklearn.utils import shuffle
-import preprocessing
+from preprocessing import preprocess, flip_image
 import matplotlib.pyplot as plt
 
 csv_data = csv('driving_log.csv')
 csv_data.columns = ['Center_Images', 'Left_Images', 'Right_Images', 'Steering_Angle', 'Throttle', 'Break', 'Speed']
 
-X_train = np.zeros(shape=(csv_data.shape[0], 40, 40, 1))
-X_flipped = np.zeros(shape=X_train.shape)
-y_train = csv_data['Steering_Angle']
-y_flipped = y_train * -1.0
+X_train = np.empty(shape=(0, 40, 40, 1))
+y_train = np.array([])
 
 
-for i, image_path in enumerate(csv_data['Center_Images']):
-    image = imread(image_path, mode='RGB')
-    image = preprocessing.preprocess(image)
-    flipped_image = preprocessing.flip_image(image)
-    X_train[i] = image
-    X_flipped[i] = flipped_image
+for i in range(len(csv_data)):
+    if i % 1000 == 0:
+        print(i/len(csv_data))
 
-X_train = np.append(X_train, X_flipped, axis=0)
-y_train = np.append(y_train, y_flipped)
+    center_steer = csv_data['Steering_Angle'][i]
+    
+    if not (center_steer == 0 and random.random() > 0.1):
+        center_image = preprocess(imread(csv_data['Center_Images'][i], mode='RGB'))
+        left_image = preprocess(imread(csv_data['Left_Images'][i], mode='RGB'))
+        left_steer = center_steer + 0.3
+        right_image = preprocess(imread(csv_data['Right_Images'][i], mode='RGB'))
+        right_steer = center_steer - 0.3
+        flipped_center_image = flip_image(center_image)
+        flipped_center_steer = -1.0 * center_steer
+        flipped_left_image = flip_image(left_image)
+        flipped_left_steer = -1.0 * left_steer
+        flipped_right_image = flip_image(right_image)
+        flipped_right_steer = -1.0 * right_steer
+
+        X_train = np.append(X_train, np.array([center_image, left_image, right_image, flipped_center_image, flipped_left_image, flipped_right_image]), axis=0)
+        y_train = np.append(y_train, np.array([center_steer, left_steer, right_steer, flipped_center_steer, flipped_left_steer, flipped_right_steer]))
+
 
 X_train, y_train = shuffle(X_train, y_train)
 
@@ -53,19 +65,7 @@ model.add(Dense(1))
 model.compile(optimizer="adam", loss="mse")
 
 
-
-# model = Sequential()
-# model.add(Convolution2D(64, 3, 3, border_mode='same', dim_ordering='tf', input_shape=(40, 40, 1)))
-# model.add(Convolution2D(64, 3, 3, border_mode='same', dim_ordering='tf'))
-# model.add(Flatten())
-# model.add(Dense(output_dim=64))
-# model.add(Activation("relu"))
-# model.add(Dropout(p=0.5))
-# model.add(Dense(output_dim=1))
-
-# model.compile(loss='mean_squared_error', optimizer=SGD(lr=0.01, momentum=0.9, nesterov=True))
-
-model.fit(X_train, y_train, nb_epoch=200, batch_size=256)
+model.fit(X_train, y_train, nb_epoch=25, batch_size=256)
 with open('model.json', mode='w', encoding='utf8') as f:
     f.write(model.to_json())
 
